aa101df957fa7acb93263fbe1bc03dce2ce9577b
Jack Pendleton <jack.n.pendleton@gmail.com>
2020-01-02 13:12:28 +0100

    :config: fix source-paths and dependencies

--- project.clj
+++ project.clj
@@ -9,13 +9,15 @@
   :main ^:skip-aot my-shadow-reagent.server
   :clean-targets ^{:protect false}
   [:target-path "target/%s"]
-  :source-paths ["src/clj" "src/cljc"]
+  :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :resource-paths ["resources"]
 
   :plugins [[lein-environ "1.1.0"]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
+                 [reagent "0.9.0-rc3"]
+                 [reagent-utils "0.3.3"]
                  [ring "1.8.0"]
                  [ring-server "0.5.0"]
                  [ring/ring-core "1.6.3"]
@@ -23,7 +25,8 @@
                  [ring/ring-jetty-adapter "1.6.3"]
                  [yogthos/config "1.1.6"]]
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.11"]
+  :profiles {:dev {:dependencies [[thheller/shadow-cljs "2.8.37"]
+                                  [binaryage/devtools "0.9.11"]
                                   [figwheel-sidecar "0.5.19"]
                                   [ring/ring-devel "1.8.0"]
                                   [ring/ring-mock "0.4.0"]
